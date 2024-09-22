29c871ed2784ddc42efc70f5e6d44ebccf29e005
Tomasz Kula <tomaszkulewicz@gmail.com>
2024-09-22 15:07:32 +0200

    fix2

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,22 @@
 /** @type {import('next').NextConfig} */
+// Konfiguracja Next.js
 const nextConfig = {
+  // Włączenie trybu ścisłego Reacta, który pomaga w wykrywaniu potencjalnych problemów w aplikacji
   reactStrictMode: true,
+  // Opcje eksperymentalne Next.js (obecnie puste)
   experimental: {},
+  // Konfiguracja obsługi zdalnych obrazów
   images: {
     remotePatterns: [
       {
+        // Wzorzec dla obrazów z via.placeholder.com
         protocol: "https",
         hostname: "via.placeholder.com",
         port: "",
         pathname: "/**",
       },
       {
+        // Wzorzec dla obrazów z img.youtube.com
         protocol: "https",
         hostname: "img.youtube.com",
         port: "",
@@ -20,4 +26,5 @@ const nextConfig = {
   },
 };
 
+// Eksportowanie konfiguracji Next.js
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,38 @@
 import type { Config } from "tailwindcss";
 
+// Konfiguracja Tailwind CSS
 const config: Config = {
+  // Ścieżki do plików, w których Tailwind CSS będzie szukać klas do generowania stylów
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", // Pliki w katalogu pages
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // Pliki w katalogu components
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Pliki w katalogu app
   ],
   theme: {
     extend: {
+      // Definicje niestandardowych tła gradientowych
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))", // Gradient radialny
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))", // Gradient stożkowy
       },
+      // Definicje niestandardowych animacji
       animation: {
-        'fade-in-up': 'fadeInUp 0.3s ease-out',
+        'fade-in-up': 'fadeInUp 0.3s ease-out', // Animacja fade-in-up
       },
+      // Definicje kluczowych klatek dla animacji
       keyframes: {
         fadeInUp: {
-          '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+          '0%': { opacity: '0', transform: 'translateY(10px)' }, // Początkowy stan animacji
+          '100%': { opacity: '1', transform: 'translateY(0)' }, // Końcowy stan animacji
         },
       },
     },
   },
+  // Wtyczki Tailwind CSS
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/aspect-ratio'), // Wtyczka do obsługi proporcji
   ],
 };
+
 export default config;
