a228a1d2daf32fd01d67596c4566fc494ca86200
Tomasz Kula <tomaszkulewicz@gmail.com>
2024-09-24 17:34:28 +0200

    test nava

--- src/components/NavContent.tsx
+++ src/components/NavContent.tsx
@@ -2,16 +2,14 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useSelector, useDispatch } from "react-redux";
-import { logout, login } from "../store/slices/authSlice";
+import { logout } from "../store/slices/authSlice";
 import { RootState } from "../store/slices/types";
-import { FaMusic } from "react-icons/fa"; // Dodaj import ikony
+import { FaMusic } from "react-icons/fa";
 
 export function NavContent() {
   const [isLoading, setIsLoading] = useState(true);
-  const { data: session, status } = useSession();
   const router = useRouter();
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(
@@ -19,33 +17,16 @@ export function NavContent() {
   );
   const user = useSelector((state: RootState) => state.auth.user);
 
-  const handleLogout = async () => {
+  const handleLogout = () => {
     console.log("Logout initiated");
-    await signOut({ redirect: false });
     dispatch(logout());
     router.push("/");
     console.log("Logout completed");
   };
 
   useEffect(() => {
-    if (status === "loading") {
-      setIsLoading(true);
-    } else {
-      setIsLoading(false);
-      if (status === "authenticated" && session?.user) {
-        dispatch(login({
-          user: {
-            id: session.user.id || '',
-            email: session.user.email || null,
-            name: session.user.name || null,
-            role: session.user.role || null
-          }
-        }));
-      } else if (status === "unauthenticated") {
-        dispatch(logout());
-      }
-    }
-  }, [status, session, dispatch]);
+    setIsLoading(false);
+  }, []);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -56,7 +37,7 @@ export function NavContent() {
       <div className="text-2xl font-bold">
         <Link href="/">MyApp</Link>
       </div>
-      <ul className="flex space-x-4 items-center"> {/* Dodano items-center */}
+      <ul className="flex space-x-4 items-center">
         <li>
           <Link href="/muzyka" className="flex items-center hover:text-gray-300">
             <FaMusic className="mr-1" />
