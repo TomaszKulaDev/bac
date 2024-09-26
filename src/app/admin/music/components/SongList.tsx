b5c717c36e023a315f4490177087bd36962fcd3b
Tomasz Kula <tomaszkulewicz@gmail.com>
2024-09-26 23:18:25 +0200

    test

--- src/app/admin/music/components/SongList.tsx
+++ src/app/admin/music/components/SongList.tsx
@@ -1,3 +1,4 @@
+// src/app/admin/music/components/SongList.tsx
 import React from "react";
 import { Song } from "@/app/muzyka/types";
 
@@ -13,20 +14,35 @@ const SongList: React.FC<SongListProps> = ({ songs, onDelete }) => {
       <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
         <thead className="bg-gray-100">
           <tr>
-            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">Nr</th>
-            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">Tytuł</th>
-            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">Artysta</th>
-            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">YouTube ID</th>
-            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">Akcje</th>
+            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+              Nr
+            </th>
+            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+              Tytuł
+            </th>
+            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+              Artysta
+            </th>
+            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+              YouTube ID
+            </th>
+            <th className="py-1 px-2 text-left text-sm font-medium text-gray-700 uppercase tracking-wider">
+              Akcje
+            </th>
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           {songs.map((song, index) => (
-            <tr key={song.id} className="hover:bg-gray-50 transition duration-150 ease-in-out">
+            <tr
+              key={song.id}
+              className="hover:bg-gray-50 transition duration-150 ease-in-out"
+            >
               <td className="py-2 px-2 text-sm text-gray-900">{index + 1}</td>
               <td className="py-2 px-2 text-sm text-gray-900">{song.title}</td>
               <td className="py-2 px-2 text-sm text-gray-900">{song.artist}</td>
-              <td className="py-2 px-2 text-sm text-gray-900">{song.youtubeId}</td>
+              <td className="py-2 px-2 text-sm text-gray-900">
+                {song.youtubeId}
+              </td>
               <td className="py-2 px-2 text-sm text-gray-900">
                 <button
                   onClick={() => onDelete(song.id)}
