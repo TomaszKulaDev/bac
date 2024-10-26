a96a59919aa1b17ff3778a52ea34185e886bebd4
Tomasz Kula <tomaszkulewicz@gmail.com>
2024-10-26 10:56:22 +0200

    fix opisu

--- src/app/constants/zIndex.ts
+++ src/app/constants/zIndex.ts
@@ -6,9 +6,10 @@
  * Użycie stałych wartości z-index pomaga w utrzymaniu spójności i przewidywalności
  * zachowania elementów nakładających się w interfejsie użytkownika.
  * 
- * MODAL_OVERLAY (30): Warstwa przyciemniająca tło podczas wyświetlania modalu.
- * MODAL (40): Sam modal, wyświetlany nad warstwą przyciemniającą.
- * TOAST (50): Powiadomienia typu toast, wyświetlane na samym wierzchu.
+ * PLAYER (30): Odtwarzacz muzyki, wyświetlany nad większością elementów strony.
+ * MODAL_OVERLAY (40): Warstwa przyciemniająca tło podczas wyświetlania modalu.
+ * MODAL (50): Sam modal, wyświetlany nad warstwą przyciemniającą.
+ * TOAST (60): Powiadomienia typu toast, wyświetlane na samym wierzchu.
  * 
  * Dzięki tym stałym, możemy łatwo zarządzać kolejnością wyświetlania elementów
  * i uniknąć konfliktów z nakładającymi się komponentami w aplikacji.
