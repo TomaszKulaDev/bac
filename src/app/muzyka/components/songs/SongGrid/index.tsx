4a02084e4f227f6047d29e7ab11f1ae4db74bcfa
Tomasz Kula <tomaszkulewicz@gmail.com>
2024-11-09 16:38:21 +0100

    poprawa kolumn

--- src/app/muzyka/components/songs/SongGrid/index.tsx
+++ src/app/muzyka/components/songs/SongGrid/index.tsx
@@ -34,7 +34,7 @@ const SongGrid: React.FC<SongGridProps> = ({ songs, ...props }) => {
       <AnimatePresence mode="wait">
         <motion.div
           layout
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6"
         >
           {filteredSongs.map((song) => (
             <motion.div
