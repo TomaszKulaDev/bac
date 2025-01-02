050a6776820503a419e08bb1ff9c9019a99ad7b0
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-01-02 17:15:05 +0100

    usuniecie spript

--- src/scripts/analyzeProject.mjs
+++
@@ -1,5 +0,0 @@
-import { generateProjectStructureReport } from "../utils/projectAnalyzer.js";
-
-console.log("Generowanie pełnego raportu projektu...");
-generateProjectStructureReport();
-console.log("Raport został wygenerowany.");
