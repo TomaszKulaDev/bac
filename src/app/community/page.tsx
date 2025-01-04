112a78a4183160932c1174819aaf9757be21749a
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-01-04 22:01:47 +0100

    czyszczenie

--- src/app/community/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Community() {
-  return <h1 className="text-4xl font-bold text-blue-500">Admin Module</h1>;
-}
--- src/app/mentorship/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Mentorship() {
-  return <h1 className="text-4xl font-bold text-blue-500">Admin Module</h1>;
-}
