1fab0b54ba77e436712fb21a268216f7bb56e7f6
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-01-08 20:15:53 +0100

    invite baner

--- src/app/szukam-partnera-do-tanca/page.tsx
+++ src/app/szukam-partnera-do-tanca/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-
 import { HeroSection } from "./components/HeroSection";
 import { LatestProfiles } from "./components/LatestProfiles";
+import { InviteBanner } from "./components/InviteBanner";
 import { QuickAds } from "./components/QuickAds";
 import { FilterProvider } from "./context/FilterContext";
 
@@ -14,6 +14,7 @@ export default function PartnerSearchPage() {
         <section className="max-w-[2200px] mx-auto px-4 sm:px-8 py-16">
           <div>
             <QuickAds />
+            <InviteBanner />
             <LatestProfiles />
           </div>
         </section>
