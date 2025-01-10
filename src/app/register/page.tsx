bbf43564b3649e9ee26a391ad3b74269bf5f7714
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-01-10 19:11:02 +0100

    login register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -141,7 +141,7 @@ export default function Register() {
 
     setIsLoading(true);
     try {
-      const response = await fetch("/api/register", {
+      const response = await fetch("/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
