70f0c414143497a28d5a5620d1692013ea951c70
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-01-13 21:18:09 +0100

    usuniecie

--- src/app/szukam-partnera-do-tanca/page.tsx
+++ src/app/szukam-partnera-do-tanca/page.tsx
@@ -3,7 +3,7 @@
 import { HeroSection } from "./components/HeroSection";
 import { LatestProfiles } from "./components/LatestProfiles";
 import { InviteBanner } from "./components/InviteBanner";
-import { QuickAds } from "./components/QuickAds";
+
 import { FilterProvider } from "./context/FilterContext";
 
 export default function PartnerSearchPage() {
@@ -12,8 +12,7 @@ export default function PartnerSearchPage() {
       <div className="min-h-screen">
         <HeroSection />
         <section className="max-w-[2200px] mx-auto px-4 sm:px-8 py-16">
-          <div>
-            <QuickAds />
+          <div>   
             <InviteBanner />
             <LatestProfiles />
           </div>
