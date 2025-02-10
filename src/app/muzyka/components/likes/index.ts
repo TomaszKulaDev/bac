e855fed7da5846467ab678330d86b0323fd16575
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-02-10 18:51:34 +0100

    usuwamy buga

--- src/app/muzyka/components/likes/index.ts
+++ src/app/muzyka/components/likes/index.ts
@@ -1,3 +1,2 @@
 export { LikedByAvatars } from "./avatars/LikedByAvatars";
 export type { LikedByUser, LikedByAvatarsProps } from "./types/likedBy";
-export { TEMP_LIKED_BY } from "./data/mockData";
