4c6c529cb5bff7e335b071d777b76f983afad4b2
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-02-14 22:28:46 +0100

    przed edyjca

--- src/app/szukam-partnera-do-bachaty/page.tsx
+++ src/app/szukam-partnera-do-bachaty/page.tsx
@@ -2,20 +2,19 @@
 
 import { LatestProfiles } from "./components/LatestProfiles";
 import { FilterProvider } from "./context/FilterContext";
-import { PromoBanner } from "./components/PromoBanner";
 
 export default function PartnerSearchPage() {
   return (
     <FilterProvider>
       <div className="min-h-screen">
-        <PromoBanner />
         {/* Sekcja z profilami */}
         <section
           className="max-w-[2200px] mx-auto px-4 sm:px-8 py-16"
           aria-label="Lista profili tancerzy"
         >
           <h1 className="sr-only">
-            Szukam Partnera do Bachaty - Znajdź Partnera do Tańca w Swojej Okolicy
+            Szukam Partnera do Bachaty - Znajdź Partnera do Tańca w Swojej
+            Okolicy
           </h1>
           <div>
             <LatestProfiles />
