d31e11900c1155113d493a838e5b2641a2f03846
Tomasz Kula <tomaszkulewicz@gmail.com>
2025-03-10 23:41:00 +0100

    fix navigations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import NewsGrid from "@/app/home/NewsGrid";
 import Footer from "@/app/home/Footer";
 import NewsGridEducation from "./home/NewsGridEducation";
 import NewsGridTopVideoDance from "./home/NewsGridTopVideoDance";
+import AdditionalNavigation from "@/app/home/components/AdditionalNavigation";
+
 export default function HomePage() {
   return (
     <div className="min-h-screen">
       <main className="pt-8">
+        {/* Additional Navigation */}
+        <AdditionalNavigation />
+
         {/* News Grid */}
         <section className="py-8">
           <NewsGrid />
